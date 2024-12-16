d3d36b1e1f536ff12c98f176bf9a21d990e6f5e7
Megalamanokaran <megalamanokaran26@gmail.com>
2024-12-16 12:10:46 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
+
 class factorial
 {
 public static void main(String args[])
 {
-Scanner obj=new Scanner(System.in);
-System.out.print("Enter a number");
-int x= obj.nextInt();
+int n=5
 int fact=1;
-for(int i=1;i<=x;i++)	
+for(int i=1;i<=n;i++)	
 fact = fact*i;
 System.out.print("factorial is:"+fact);
   system.out.print("the answer is:"+fact);
